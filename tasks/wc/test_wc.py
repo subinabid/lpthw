68dd4a38fc57f8d885cd44cdfbf9382263a6dbe5
Subin Abid <subinabid@gmail.com>
2023-01-17 22:22:05 +0530

    Simple test for wc multiple files that exists

--- tasks/wc/test_wc.py
+++ tasks/wc/test_wc.py
@@ -4,7 +4,13 @@ def test_empty():
     assert (0, 0, 0)  == wc("null.txt")
 
 def test_self():
-    assert (42, 151, 1497) == wc("wc.py")
+    assert (57, 186, 1867) == wc("wc.py")
 
 def test_file():
     assert (3, 5, 28) == wc("file1.txt")
+
+def test_wct():
+    files1 = ["wc.py", "file1.txt"]
+    files2 = ["wc.py", "null.txt"]
+    assert(60, 191, 1895) == wct(files1)
+    assert(57, 186, 1867) == wct(files2)
